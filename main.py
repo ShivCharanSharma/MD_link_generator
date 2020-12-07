fed26f5651a8dad256f15d8b7cfce920283813e3
ShivCharanSharma <shivcharanmt@gmail.com>
2020-12-07 12:33:24 +0530

    Added functionality for searching in sub directory and limiting searching by level of sub directories . Also added functionality for Top down and bottom up searching.

--- main.py
+++ main.py
@@ -2,20 +2,39 @@ import fnmatch
 import os
 import sys 
 import csv
+import click
+import math
 
-outputFileName="output_of_link.csv"
-path="./"
-if len(sys.argv)>1:
-    path=sys.argv[1]
+@click.command()
+@click.option('--level','-l',default=math.inf,help='specify levels of  searching in sub directories')
+@click.option('--bottom','-b',is_flag=True,help='Bottom up searching searching in sub directories. Default is Top to Bottom.')
+@click.option('--in-dir','-i',default='./',help='Enter path of input directory')
+def createCSV(in_dir,level,bottom):
+    assert os.path.isdir(in_dir)
+    num_dir_prefix=in_dir.count(os.path.sep)
+    #print(num_dir_prefix)
+    #print(level)
+    outputFileName="output_of_link.csv"
+    heading=['S no.','File Name','Link']
+    with open(outputFileName,'w') as csvOutput:
+        csvWriter=csv.writer(csvOutput)
+        csvWriter.writerow(heading)
+        entryCounter=1
+        # search in directory or subdirectory based on flag
+        for (root,dirs,files) in os.walk(in_dir,topdown=not bottom):
+            if num_dir_prefix + level >= root.count(os.path.sep):
+                #print(root)
+                #print(root.count(os.path.sep))
+                for f in fnmatch.filter(files,"*.md"):
+                    #create MD type link from file name
+                    link="["+f+"](Files/"+f[:-3]+".html)"
+                    # added entry in csv 
+                    csvWriter.writerow([entryCounter,f,link])
+                    entryCounter +=1
+    
 
-files=fnmatch.filter(os.listdir(path),"*.md")
-heading=['File Name','Link']
-with open(outputFileName,'w') as csvOutput:
-    csvWriter=csv.writer(csvOutput)
-    csvWriter.writerow(heading)
-    for f in files:
-        link="[f](Files/"+f[:-3]+".html)"
-        csvWriter.writerow([f,link])
+
+if __name__ == '__main__':
+    createCSV()
 
 
-print(files)
